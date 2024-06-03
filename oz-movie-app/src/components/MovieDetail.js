bae17751b90015ed99e7fb32de97147c786849e2
ungpyolee <uplee114@gmail.com>
2024-06-03 17:01:06 +0900

    mission 1

--- oz-movie-app/src/components/MovieDetail.js
+++ oz-movie-app/src/components/MovieDetail.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import axios from '../api/axios';
+import { FaStar } from "react-icons/fa";
+import styled from 'styled-components';
 
 const MovieDetail = () => {
 
@@ -8,16 +10,61 @@ const MovieDetail = () => {
     const [movie, setMovie] = useState(null)
     console.log(movieId)
 
-    const { movieDetail } = axios.get(`movie/${movieId}`)
+    useEffect(() => {
+        const fetchMovie = async () => {
+            const response = await axios.get(`/movie/${movieId}`);
+            setMovie(response.data);
+        };
 
-    console.log(movieDetail)
+        fetchMovie();
+    }, [movieId]);
 
+
+   
+    
+    // {.map((movieId) => (genre_ids)}
     return (
-        <div>
-             
-        
-        </div>
+        <>
+            {movie ? (
+                <MovieDetailWrap style={{color:'#fff'}}>
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                    <div style={{padding:'1rem'}}>
+                    <MovieCardTitle>{movie.title}</MovieCardTitle>
+                    <MovieCardAverage><FaStar style={{marginRight:'.25rem'}}/> {movie.vote_average}</MovieCardAverage>
+                    {/* <p>{movie.genre_ids}</p> */}
+                    <p>{movie.overview}</p>
+                    </div>
+                </MovieDetailWrap>
+            ) : (
+                <div>영화를 찾을 수 없습니다.</div>
+            )}
+        </>
     )
 }
+const MovieCardTitle = styled.p`
+  width:100%;
+  font-size:1.25rem;
+  font-weight:700;
+  padding-top:.5rem;
+  padding-bottom:.25rem;
+`
+const MovieCardAverage = styled.p`
+width:100%;
+  font-size:1rem;
+  text-align:center;
+  padding-bottom:.5rem;
+  display:flex;
+  
+`
+const MovieDetailWrap = styled.div`
+    border:1px soild black;
+    box-sizing:border-box;
+    display: flex;
 
-export default MovieDetail
+    img {
+        width:50%;
+        display:block;
+    }
+`
+
+export default MovieDetail;
